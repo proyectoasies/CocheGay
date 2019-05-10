953603f64db07a3211c7d58c02f6166120a1e0fe
JorgeBa3 <49853820+JorgeBa3@users.noreply.github.com>
2019-05-10 17:10:23 -0600

    Add files via upload

---
+++ referencia.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+
+	<div id="titulo" align="center">Pregunta 1</div>
+	<table id="tabla" align="center">
+		<tr>
+		<td id="texto">What is the name of the <br>
+				phenomenon where the second hand <br>
+			looks like it stops moving?
+		</td>
+		<td>
+		<td>
+			<input id="cuadrado1" type="submit" name="" value="A time paradox.">
+			<input id="cuadrado1" type="submit" name="" value="Stopwatch.">
+			<input id="cuadrado1" type="submit" name="" value="The World.">
+			<input id="cuadrado1" type="submit" name="" value="Chronostasis.">
+		</td>
+		<td><input id="flecha" type="submit" name="" value="Siguiente"></td>
+		</tr>
+
+	</table>
+
+	
+	
+</body>
+</html>
